4ba1a9f166759c1c8bfd68aa0f7213a3495b0197
Quincy Jones <rqjones@asu.edu>
2023-03-17 10:47:11 -0700

    Forward Contract Update to receive and send Assets

--- contracts/Forward.sol
+++ contracts/Forward.sol
@@ -6,6 +6,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract ASSET is ERC1155 {
+        constructor() ERC1155("https://thisissomeMetaData/{id}.json") {
+    }
+}
+
 //Smart contract
 contract Forward is ERC1155 {
     uint256 public constant forwardToken = 0;
@@ -13,8 +19,21 @@ contract Forward is ERC1155 {
     uint public realeaseDate = 0;
     uint public price = 0;
     bool public contractSold = false;
+    address public CommodityTokenAddress;
+    ASSET public Asset;
+    uint[] public CommodityTokens;
+    address[] public TokenList;
+    uint[] public TokenAmmounts;
 
-    constructor() ERC1155("https://thisissomeMetaData/{id}.json") {
+    constructor(address[] memory _CommodityTokenAddress,uint[] memory _Tokens) ERC1155("https://thisissomeMetaData/{id}.json") {
+        CommodityTokenAddress = _CommodityTokenAddress[0];
+        Asset = ASSET(CommodityTokenAddress);
+        //CommodityTokenAddress.safeTransferFrom(address(this),msg.sender,shareToken, 1, "");
+        for (uint i; i <= _Tokens.length;i++) {
+            TokenList.push(address(this));
+            TokenAmmounts.push(1);
+        }
+        require(Asset.balanceOfBatch(TokenList, _Tokens).length == _Tokens.length, "Contract must hold tokens");
         _mint(msg.sender,handelerToken,1, "");
     }
     //only the holder of the handlerToken can access functions with this modifier
@@ -46,9 +65,9 @@ contract Forward is ERC1155 {
         return true;
     }
     // token holder can redeem contract once time has elaps
-    function redeemForward()public ForwardToken returns(bool){
-
-        return true;
+    function redeemForward()public ForwardToken returns(bool,string memory){
+        Asset.safeTransferFrom(address(this), msg.sender, TokenList, TokenAmmounts, "");
+        return (true,"Forward Contract Redeemed");
     }
     //ERC1155Received fuctions
     function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
