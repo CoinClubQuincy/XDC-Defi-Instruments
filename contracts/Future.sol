b2e1e9d4f450ae87b0938b20d4e5df1856cbbead
Quincy Jones <rqjones@asu.edu>
2023-03-23 18:23:20 -0700

    return change
    as well as check if contract is in default and fixed spelling

--- contracts/Future.sol
+++ contracts/Future.sol
@@ -23,11 +23,14 @@ contract Future is ERC1155 {
     uint public Deposit; // 100 = 1% | 10 = 10% | 4 = 25% | 2 = 50% 
     uint public fee; // 1 = 0.1% 10 basis points
     uint public currentFee;
+    uint public redemptionTimeLimit; // the total amount of time before contract defaults after maturity in seconds standard 1Day- 1Week
+    bool DefaultStatus = false;
 
-    constructor(address[] memory _AssetTokenAddress,uint[] memory _Tokens,uint _divisable, uint _fee, string memory _URI) ERC1155(_URI) {
+    constructor(address[] memory _AssetTokenAddress,uint[] memory _Tokens,uint _divisable, uint _fee, string memory _URI,uint _redemptionTimeLimit) ERC1155(_URI) {
         AssetTokenAddress = _AssetTokenAddress[0];
         Tokens = _Tokens;
-        Deposit = _divisable;
+        Deposit = price/_divisable;
+        redemptionTimeLimit = _redemptionTimeLimit;
         fee = _fee;
         currentFee = (price/1000) * _fee;
         Asset = ASSET(AssetTokenAddress);
@@ -39,14 +42,24 @@ contract Future is ERC1155 {
         _mint(msg.sender,handelerToken,1, "");
     }
     //only the holder of the handlerToken can access functions with this modifier
-    modifier Handeler{
-        require(balanceOf(msg.sender,handelerToken) == 1,"you are currently not the handeler of this contract");
+    modifier Handler{
+        require(balanceOf(msg.sender,handelerToken) == 1,"you are currently not the handler of this contract");
         _;
     }
     //only the holder of the FutureToken can access functions with this modifier
     modifier FutureToken{
-        require(balanceOf(msg.sender,futureToken) == 1,"you currently do not have custody of this contracts token");
+        require(balanceOf(msg.sender,futureToken) == 1,"yoou currently do not have custody of this contracts token");
         require(block.timestamp >= realeaseDate,"the date of maturity has not passed");
+        _;
+    }
+    modifier PurchaseAsset{
+        if(redemptionTimeLimit + realeaseDate <= block.timestamp){
+            DefaultStatus = true;
+        }
+        returnChange(price);
+        require(DefaultStatus = false, "Contract in Default");
+        require(msg.value >= price,"not enough funds");
+        require(block.timestamp >= realeaseDate && redemptionTimeLimit + realeaseDate >= block.timestamp,"the date of maturity has not passed");
         _;
     }
     // if token has been bought user cant execute the fuction with this modifire
@@ -54,8 +67,13 @@ contract Future is ERC1155 {
         require(contractSold == false,"contract has been sold");
         _;
     }
+    function returnChange(uint _total)internal{
+        if(msg.value>_total){
+            payable(msg.sender).transfer(msg.value - _total);
+        }
+    }
     // Crreate func to list token price and drop commodities token in futures contract | as well as collatoral limit
-    function listSale(uint _realeaseDate, uint _price) private Handeler bought returns(bool){
+    function listSale(uint _realeaseDate, uint _price) public Handler bought returns(bool){
         realeaseDate = _realeaseDate;
         price = _price;
         return true;
@@ -63,20 +81,18 @@ contract Future is ERC1155 {
     // create sales function to but contract 
     function initiateContract()public payable bought returns(bool){
         require(msg.value>=Deposit+currentFee,"funds note enough to purchace contract");
-        if(msg.value>price){
-            payable(msg.sender).transfer(msg.value - price);
-        }
+        returnChange(Deposit+currentFee);
         _mint(msg.sender,futureToken,1, "");
         contractSold = true;
         return true;
     }
     // token holder can redeem contract once time has elaps
-    function BuyAsset()public payable FutureToken returns(bool,string memory){
-        require(msg.value == price - Deposit,"not enough funds");
+    function BuyAsset()public payable FutureToken PurchaseAsset returns(bool,string memory){
+        payable(msg.sender).transfer(Deposit);
         Asset.safeBatchTransferFrom(address(this), msg.sender, Tokens, TokenAmmounts, "0x0");
         return (true,"Future Contract Redeemed");
     }
-    function redeemDeposit()public payable Handeler returns(bool,string memory){
+    function redeemDeposit()public payable Handler returns(bool,string memory){
         require(contractSold = true,"contract not sold");
         payable(msg.sender).transfer(Deposit + currentFee);
         return (true,"Future Contract Redeemed");
