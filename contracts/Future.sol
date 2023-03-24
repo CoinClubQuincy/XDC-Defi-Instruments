2872adf400eb57ec7a0a9375da03c6956025d25a
Quincy Jones <rqjones@asu.edu>
2023-03-23 17:48:08 -0700

    Futures contract complete

--- contracts/Future.sol
+++ contracts/Future.sol
@@ -3,14 +3,39 @@
 pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 //Smart contract
+contract ASSET is ERC1155 {
+        constructor() ERC1155("https://thisissomeMetaData/{id}.json") {
+    }
+} 
+//Smart contract
 contract Future is ERC1155 {
     uint256 public constant futureToken = 0;
     uint256 public constant handelerToken = 0;
     uint public realeaseDate = 0;
     uint public price = 0;
     bool public contractSold = false;
+    address public AssetTokenAddress;
+    ASSET public Asset;
+    uint[] public AssetTokens;
+    address[] public TokenList;
+    uint[] public TokenAmmounts;
+    uint[] public Tokens;
+    uint public Deposit; // 100 = 1% | 10 = 10% | 4 = 25% | 2 = 50% 
+    uint public fee; // 1 = 0.1% 10 basis points
+    uint public currentFee;
 
-    constructor() ERC1155("https://thisissomeMetaData/{id}.json") {
+    constructor(address[] memory _AssetTokenAddress,uint[] memory _Tokens,uint _divisable, uint _fee) ERC1155("https://thisissomeMetaData/{id}.json") {
+        AssetTokenAddress = _AssetTokenAddress[0];
+        Tokens = _Tokens;
+        Deposit = _divisable;
+        fee = _fee;
+        currentFee = (price/1000) * _fee;
+        Asset = ASSET(AssetTokenAddress);
+        for (uint i; i <= _Tokens.length;i++) {
+            TokenList.push(address(this));
+            TokenAmmounts.push(1);
+        }
+        require(Asset.balanceOfBatch(TokenList, _Tokens).length == _Tokens.length, "Contract must hold tokens");
         _mint(msg.sender,handelerToken,1, "");
     }
     //only the holder of the handlerToken can access functions with this modifier
@@ -29,22 +54,32 @@ contract Future is ERC1155 {
         require(contractSold == false);
         _;
     }
-    // Crreate func to list token price and drop commodities token in Futures contract | as well as collatoral limit
-    function listSale(uint _realeaseDate, uint _price) public Handeler bought returns(bool){
+    // Crreate func to list token price and drop commodities token in futures contract | as well as collatoral limit
+    function listSale(uint _realeaseDate, uint _price) private Handeler bought returns(bool){
         realeaseDate = _realeaseDate;
         price = _price;
         return true;
     }
     // create sales function to but contract 
-    function buyFutureToken()public payable  bought returns(bool){
+    function initiateContract()public payable bought returns(bool){
+        require(msg.value>=Deposit+currentFee,"funds note enough to purchace contract");
+        if(msg.value>price){
+            payable(msg.sender).transfer(msg.value - price);
+        }
         _mint(msg.sender,futureToken,1, "");
         contractSold = true;
         return true;
     }
     // token holder can redeem contract once time has elaps
-    function redeemFuture()public FutureToken returns(bool){
-
-        return true;
+    function BuyAsset()public payable FutureToken returns(bool,string memory){
+        require(msg.value == price - Deposit,"not enough funds");
+        Asset.safeBatchTransferFrom(address(this), msg.sender, Tokens, TokenAmmounts, "0x0");
+        return (true,"Future Contract Redeemed");
+    }
+    function redeemDeposit()public payable Handeler returns(bool,string memory){
+        require(contractSold = true,"contract not sold");
+        payable(msg.sender).transfer(Deposit + currentFee);
+        return (true,"Future Contract Redeemed");
     }
     //ERC1155Received fuctions
     function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
