45c5ed65d06f7b6a0021cf1fd659d39e3d13b71b
Quincy Jones <rqjones@asu.edu>
2023-03-29 10:13:34 -0700

    minor details change name
    added return functions to functions without returns

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 //Smart contract
-contract Forward is ERC1155 {
+contract Contract is ERC1155 {
     uint256 public constant PartyA = 0;
     uint256 public constant PartyB = 1;
 
@@ -40,7 +40,7 @@ contract Forward is ERC1155 {
     }
 
     //this function will compare any data of hash to see if it matches the DocumentHash
-    function verify(string memory _DocumentHash)public returns(bool){
+    function verify(string memory _DocumentHash)public view returns(bool){
         if(keccak256(abi.encodePacked(_DocumentHash)) == keccak256(abi.encodePacked(DocumentHash)) ){
             return true;
         } else {
@@ -48,13 +48,15 @@ contract Forward is ERC1155 {
         }
     }
     // Party A or party B can make a counter proposal and  reassign the hash
-    function counter_proposal_A(string memory _DocumentHash)public PartyA_Sign returns(bool){
+    function counter_proposal_A(string memory _DocumentHash)public  PartyA_Sign returns(bool){
         DocumentHash = _DocumentHash;
         total_Counter_Proposals++;
+        return true;
     }
     function counter_proposal_B(string memory _DocumentHash)public PartyB_Sign returns(bool){
         DocumentHash = _DocumentHash;
         total_Counter_Proposals++;
+        return true;
     }
     //Both parties can sign an make the contract official
     function sign_proposal()public contractSigned returns(bool,string memory){
@@ -67,6 +69,8 @@ contract Forward is ERC1155 {
         }
         if(PartyASignature != address(0) && PartyBSignature != address(0) ){
             contractComplete = true;
+            return (true, "only one party  has signed");
         }
+        return (false, "error user does not hold token to authorize this call");
     }
 }
