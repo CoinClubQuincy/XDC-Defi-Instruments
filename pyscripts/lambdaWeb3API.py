c82768343e737f92fdb1cc321dbcff9d201d7f8f
Quincy Jones <rqjones@asu.edu>
2023-10-02 07:36:17 -0700

    Update lambdaWeb3API.py

--- pyscripts/lambdaWeb3API.py
+++ pyscripts/lambdaWeb3API.py
@@ -1,57 +1,83 @@
 import sha3
-from web3 import Web3
-from eth_account import Account
-from eth_account.signers.local import LocalAccount
-from web3 import Web3, EthereumTesterProvider
-from web3.middleware import construct_sign_and_send_raw_middleware
+from web3 import Web3, AsyncWeb3
 
-address = "0x0000000000000000000000000000000000000000"
-network = "XDC"
-amount = 0
-sendTo = "BLANK"
-tokenID = [0]
+class executeWeb3:
+    w3 = None
+    network = None
+    userAddress = None
 
+    def __init__(self,network,userAddress):
+        self.network = network
+        self.userAddress = userAddress
 
-private_key = "this is suposed to be a word key"
+        print("Joining: [ %s ] network, with Address: %s" % (self.network,self.userAddress ))
 
+        w3 = Web3(Web3.HTTPProvider(self.network))
+        self.w3 = w3
+        print(self.w3.is_connected())
+        self.balance(userAddress)
 
+    def balance(self,address):
+        balance = self.w3.eth.get_balance(address)
+        print(balance)
+        return balance
 
-class NetworkTXNs()
-
-    init():
-        w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
-
-
-    def Send():
+    def send(amount,network,sendTo):
+        w3.send(self)
         print("send %s %s to %s", (amount,network,sendTo))
         #web3.eth.send_transaction(sendTo,,amount)
 
-    def SendXRC20():
+    def sendXRC20(self,amount,address,sendTo):
+        abi = ""
+        contract = w3.eth.contract(address=address, abi=abi)
+
+
         print("sendXRC20 %s %s to %s", (amount,network,sendTo))
 
-    def SendXRC1155():
+    def sendXRC1155(self,amount,address,sendTo):
+        abi = ""
+        contract = w3.eth.contract(address=address, abi=abi)
+
+
         print("sendXRC1155 %s %s to %s", (amount,network,sendTo))
 
-    def ExecuteContract():
-        print("sendXRC1155 %s %s to %s", (amount,network,sendTo))
+    def setPrivateKey(self,seed):
+        print("set private key")
+
+    def retrivePrivateKey():
+        print("set private key")
+
+    def call(self,url):
+        address, abi = getCIML(url)
+        contract = w3.eth.contract(address=address, abi=abi)
+
+        ##contract calls
+
+        print("call Address: %s ABI: %s to %s", (address,abi))
+
+    def getCIML(self,url):
+        #S3 get url
+        #parse json data for 
+        address = ""
+        abi = ""
+        return address, abi
+
+class parseJSON:
+
+    json = None
+    def __init__(_json):
+        json = _json
+
+    
 
 if __name__ == "__main__" :
-    print("start program")
-    password = input("Enter a password: ")
+    address = "0xD69B4e5e5A7D5913Ca2d462810592fcd22F6E003"
 
+    print("start program from: %s" % address)
 
     k = sha3.keccak_256()
-    data = 'age'.encode('utf-8')  # Encode the string 'age' into bytes
+    data = 'age'.encode('utf-8')  
     k.update(data)
     #print(k.hexdigest())
 
-    enter = input("Enter a value: ")
-    
-    if enter == "1":
-        Send()
-    elif enter == "2":
-        SendXRC20()
-    elif enter == "3":
-        SendXRC1155()
-    else:
-        print("Error")
+    exe = executeWeb3("http://127.0.0.1:8545",address)
