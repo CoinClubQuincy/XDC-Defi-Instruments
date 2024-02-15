45c7e26e7b482ec0e7ed7f042d6e5bbd6ec9c55e
Quincy Jones <rqjones@asu.edu>
2024-02-15 09:38:30 -0700

    co pilot adjusted test

--- test/Asset.js
+++ test/Asset.js
@@ -1,31 +1,47 @@
-const { 
-  time, 
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Asset", function () {
+  let Asset, asset, owner, addr1, addr2;
 
-    async function AssetContractLoad() {
-      const [owner, otherAccount] = await ethers.getSigners();
-      const URI = "{ Test URI }"
-      const Asset = await ethers.getContractFactory("Asset");
-      const asset = await Asset.deploy(URI);
+  beforeEach(async function () {
+    Asset = await ethers.getContractFactory("Asset");
+    [owner, addr1, addr2, _] = await ethers.getSigners();
+    asset = await Asset.deploy("https://thisissomeMetaData/{id}.json");
+    await asset.deployed();
+  });
 
-      return { asset, owner, otherAccount };
-    }
-  
-    describe("Deploy Contract", async function () {
-      const { asset } = await loadFixture(AssetContractLoad);
-      const totalCoins = await asset.totalCoins();
-
-      expect(totalCoins).to.equal(0);
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await asset.balanceOf(owner.address, 0)).to.equal(1);
     });
 
-    it("viewToken", async function () {
-      const { asset } = await loadFixture(AssetContractLoad);
-
-      expect(asset.totalCoins()).equal(0);
+    it("Should reject empty URI", async function () {
+      const AssetEmpty = await ethers.getContractFactory("Asset");
+      await expect(AssetEmpty.deploy("")).to.be.revertedWith("URI cannot be empty");
     });
   });
+
+  describe("Transactions", function () {
+    it("Should create new token", async function () {
+      await asset.AddToken("Token1", "Attribute1", "Attribute2", "AssetType1");
+      const token = await asset.viewToken(0);
+      expect(token.name).to.equal("Token1");
+      expect(token.description).to.equal("Attribute1");
+      expect(token.image).to.equal("Attribute2");
+      expect(token.assetType).to.equal("AssetType1");
+    });
+
+    it("Should move asset", async function () {
+      await asset.AddToken("Token1", "Attribute1", "Attribute2", "AssetType1");
+      await asset.moveAsset(0, owner.address, addr1.address, 1);
+      expect(await asset.balanceOf(addr1.address, 0)).to.equal(1);
+    });
+
+    it("Should destroy asset", async function () {
+      await asset.AddToken("Token1", "Attribute1", "Attribute2", "AssetType1");
+      await asset.destroyAsset(0, owner.address);
+      expect(await asset.balanceOf(owner.address, 0)).to.equal(0);
+    });
+  });
+});
