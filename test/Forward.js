f869d5292ba6b2e8a9ee7cfe4d2a9905e4a6e192
Quincy Jones <rqjones@asu.edu>
2024-02-22 21:32:08 -0700

    Forward test

--- test/Forward.js
+++ test/Forward.js
@@ -1,37 +1,58 @@
-// const { time, loadFixture,} = require("@nomicfoundation/hardhat-network-helpers");
-// const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-// const { expect } = require("chai");
-  
-//   before("Forward Contract", function () {
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-//     async function init() {
+describe("Forward", function () {
+  let ASSET, Forward;
+  let asset, forward;
+  let owner, buyer, addr3, ...addrs;
 
-//     }
-  
-//     describe("Contract Unit Test", function () {
-//       it("FuncA", async function () {
-//         const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-  
-//         expect(await lock.unlockTime()).to.equal(unlockTime);
-//       });
-  
-//       it("FuncB", async function () {
-//         const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-  
-//         expect(await lock.owner()).to.equal(owner.address);
-//       });
-//     });
-//     describe("Contract Functional Test", function () {
-//       it("FuncA", async function () {
-//         const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-  
-//         expect(await lock.unlockTime()).to.equal(unlockTime);
-//       });
-  
-//       it("FuncB", async function () {
-//         const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-  
-//         expect(await lock.owner()).to.equal(owner.address);
-//       });
-//     });
-// })
+  beforeEach(async function () {
+    ASSET = await ethers.getContractFactory("ASSET");
+    Forward = await ethers.getContractFactory("Forward");
+    [owner, buyer, addr3, ...addrs] = await ethers.getSigners();
+
+    asset = await ASSET.deploy();
+    await asset.deployed();
+
+    forward = await Forward.deploy("https://example.com");
+    await forward.deployed();
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await forward.owner()).to.equal(owner.address);
+    });
+  });
+
+  describe("Transactions", function () {
+    it("Should deploy assets", async function () {
+      await forward.deployAssets(asset.address, 100, Date.now() + 10000, ethers.utils.parseEther("1"));
+      expect(await forward.AssetTokenAddress()).to.equal(asset.address);
+      expect(await forward.Tokens()).to.equal(100);
+      expect(await forward.realeaseDate()).to.be.gt(Date.now());
+      expect(await forward.price()).to.equal(ethers.utils.parseEther("1"));
+    });
+
+    it("Should buy forward token", async function () {
+      await forward.deployAssets(asset.address, 100, Date.now() + 10000, ethers.utils.parseEther("1"));
+      await forward.connect(buyer).buyForwardsToken({ value: ethers.utils.parseEther("1") });
+      expect(await forward.balanceOf(buyer.address, 0)).to.equal(1);
+    });
+
+    it("Should redeem forward", async function () {
+      await forward.deployAssets(asset.address, 100, Date.now() + 10000, ethers.utils.parseEther("1"));
+      await forward.connect(buyer).buyForwardsToken({ value: ethers.utils.parseEther("1") });
+      await ethers.provider.send("evm_increaseTime", [10000]);  // Increase time to pass the release date
+      await ethers.provider.send("evm_mine");  // Mine the next block
+      await forward.connect(buyer).redeemForward();
+      expect(await asset.balanceOf(buyer.address, 100)).to.equal(1);
+    });
+
+    it("Should redeem contract value", async function () {
+      await forward.deployAssets(asset.address, 100, Date.now() + 10000, ethers.utils.parseEther("1"));
+      await forward.connect(buyer).buyForwardsToken({ value: ethers.utils.parseEther("1") });
+      await forward.redeemContractValue();
+      expect(await ethers.provider.getBalance(forward.address)).to.equal(0);
+    });
+  });
+});
